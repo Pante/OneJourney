3eb39fb6ecf15548917a0cf01bb073506c1e0679
Matthias Ngeo <matthiasngeozhongsim@yahoo.com>
2018-12-30 23:53:31 +0800

    Added reward backend (#91)
    * Fixed failing lint
    
    * Finished backend for view rewards page
    
    TODO - refactor events & rewards interceptors
    TODO - map event & reward enums
    TODO - refactor events & rewards component -> possible manged
    observable?

--- src/app/main/rewards/reward-service.ts
+++ src/app/main/rewards/reward-service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Event } from './event';
+import { Reward } from './reward';
 import { AuthenticationService } from '../../authentication/authentication-service';
 import { RESTListService } from 'src/app/shared/rest/rest-list-service';
 
 
 @Injectable({ providedIn: 'root' })
-export class EventService extends RESTListService<Event> {
+export class RewardService extends RESTListService<Reward> {
     
     constructor(authentication: AuthenticationService, http: HttpClient) {
-        super(authentication, http, `activities?id=${authentication.identity.id}`);
+        super(authentication, http, `reward_catelogues?id=${authentication.identity.id}`);
     }
     
-    protected deserialize(data: any): Event {
-        return Event.deserialize(data);
+    protected deserialize(data: any): Reward {
+        return Reward.deserialize(data);
     }
     
 }
+
