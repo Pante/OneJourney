421fcaefc3063803f0ba77e1c6596ca5db70cf01
Sim Kim Wee <sim.kim.wee.012s@gmail.com>
2019-01-03 23:02:49 +0800

    Front-end upgrade with profile page completion

--- src/app/main/events/events.component.ts
+++ src/app/main/events/events.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewEncapsulation } from '@angular/core';
 
 import { DeviceDetectorService } from 'ngx-device-detector';
 import { Subscription } from 'rxjs';
