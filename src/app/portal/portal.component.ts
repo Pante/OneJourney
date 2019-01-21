18488948a743d10b2642cb05ab8ab0e3925902ac
Sim Kim Wee <sim.kim.wee.012s@gmail.com>
2019-01-21 12:25:30 +0800

    Sorry.....

--- src/app/portal/portal.component.ts
+++ src/app/portal/portal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../authentication/authentication.service';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-main',
@@ -16,6 +17,13 @@ export class PortalComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    $(function () {
+      $('#sidebarCollapse').on('click', function () {
+        $('#sidebar, #content').toggleClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+      });
+    });
   }
 
 }
