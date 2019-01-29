2f50b58ba12fa7eec7fe56de4c3863df77eeb110
Nicholas Chin <cwhnick1998@gmail.com>
2019-01-29 23:19:00 +0800

    Push attempt
    ts stuff

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,14 +1,64 @@
-import { AppPage } from './app.po';
+import { AppPage, getEventBtn, getProfileBadge } from './app.po';
+import { browser, element, by, By, $, $$, ExpectedConditions} from 'protractor';
 
-describe('workspace-project App', () => {
+describe('Protractor e2e test', () => {
   let page: AppPage;
-
+  const cards = element.all(by.css('app-event-card'));
+  //const tabs = element.all(by.css('app-profile-nav-item'));
+  const firstCard = cards.get(0);
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display landing page', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to one-journey!');
+    expect(page.getEventText()).toEqual('Events');
   });
-});
+
+  it('clicking on first card title', () => {
+    firstCard.click();
+    expect(firstCard.getText());
+  });
+
+  it('click on enroll', () => {
+    const enrollbtn = getEventBtn('Enroll');
+    enrollbtn.click();
+  });
+
+  it('click on unenroll', () => {
+    const unenroll = getEventBtn('Unenroll');
+    unenroll.click();
+  });
+
+  it('click on close', () => {
+    const closecard = getEventBtn('Close');
+    closecard.click();
+  });
+
+  it('should go into profile page', () => {
+    page.navigateTo();
+    //expect(page.getProfileSidebar().click());
+    page.getProfileSidebar().click();
+    expect(page.getProfileText()).toEqual('Profile');
+  });
+
+  it('should click into profile badges', () => {
+    //const profilebadge = getProfileBadge('Badges');
+    //profilebadge.click();
+    //page.getBadgesTab().click();
+    page.clickBadgesTab();
+    expect(page.getBadgesTab()).toEqual('Badges');
+  });
+
+  it('should go into leaderboard page', () => {
+    page.getLeaderboardPage();
+    expect(page.getLeaderboardText()).toEqual('Leaderboard');
+    browser.pause();
+  });
+
+  it('should go into rewards page', () => {
+    page.getRewardsPage();
+    expect(page.getRewardsText()).toEqual('Rewards');
+    browser.pause();
+  });
+});  
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,58 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
+
+
   navigateTo() {
     return browser.get('/');
   }
 
-  getTitleText() {
-    return element(by.css('app-root h1')).getText();
+  navigateToProfile() {
+    return browser.get('/portal/profile');
   }
+
+  getEventText() {
+    return element(by.css('app-events h1')).getText();
+  }
+
+  getLeaderboardText() {
+    return element(by.css('app-leaderboard h1')).getText();
+  }
+
+  getRewardsText() {
+    return element(by.css('app-rewards h1')).getText();
+  }
+
+  getProfileText() {
+    return element(by.css('app-profile h1')).getText();
+  }
+  
+  getProfileSidebar() {
+    return element(by.css('[routerlink="/portal/profile"]'));
+  }
+
+  getBadgesTab() {
+    return element(by.css('app-profile li:nth-of-type(2)')).getText();
+  }
+
+  clickBadgesTab() {
+    return element(by.css('app-profile li:nth-of-type(2)')).click();
+  }
+
+  getLeaderboardPage() {
+    return element(by.css('li:nth-of-type(4)')).click();
+  }
+
+  getRewardsPage() {
+    return element(by.css('li:nth-of-type(6)')).click();
+  }
+
 }
+
+export function getProfileBadge(tabName: string): any {
+    return element(by.id('badges-tab')).all(by.anchor(tabName));
+  }
+
+export function getEventBtn(btnName: string): any {
+    return element(by.className('modal-footer')).all(by.buttonText(btnName));
+  }
