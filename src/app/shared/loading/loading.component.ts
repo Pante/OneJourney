05164dde8aaf784bf6b36c3abe53c0bb22c354d8
Matthias Ngeo <matthiasngeo@gmail.com>
2019-01-29 17:34:19 +0800

    Commit

---
+++ src/app/shared/loading/loading.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-loading',
+  templateUrl: './loading.component.html',
+  styles: ['./loading.component.css']
+}) 
+export class LoadingComponent {
+    
+    enable: boolean;
+    title: string;
+    message: string;
+    
+    
+    
+    
+    
+}
